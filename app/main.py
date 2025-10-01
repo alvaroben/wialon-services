ee65878ce373cd54a295866a39477a76e8833ce1
Alvaro Bencosme <alvarobencosme@gmail.com>
2025-10-01 17:12:38 -0400

    add cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routers import locator_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Gurtam Microservice")
+app = FastAPI(title="Wialon Microservice")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(locator_router.router)
